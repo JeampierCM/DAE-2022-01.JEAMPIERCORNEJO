9f77312bfb3995aefb04fc67d661481109b7990b
Jeampier Cornejo <jeampier.cornejo@tecsup.edu.pe>
2022-05-11 16:54:32 -0500

    lab09

---
+++ lab09/src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      articulos: [{
+                      codigo: 1, 
+                      descripcion: 'coca-cola',
+                      precio: 2.50
+                 },{
+                      codigo: 2, 
+                      descripcion: 'inka-cola',
+                      precio: 2.20
+                 },{
+                      codigo: 3, 
+                      descripcion: 'fanta',
+                      precio: 1.70
+                 }]
+    }
+    this.borrar = this.borrar.bind(this);
+  }
+  render() {
+    return (
+      
+      <div class="container">
+      <center><div>
+      <br/>
+        <h1>Lista de articulos</h1>
+        <br/>
+        <table  class="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th>Codigo</th>
+              <th>Descripcion</th>
+              <th>Precio</th>
+              <th>Borrar</th>
+            </tr>
+          </thead>
+        
+        <tbody>
+        {this.state.articulos.map(elemento => {
+          return (
+            <tr key={elemento.codigo}>
+              <td>
+                {elemento.codigo}  
+              </td>
+              <td>
+                {elemento.descripcion}
+              </td>
+              <td>
+                {elemento.precio}
+              </td>              
+              <td>
+                <button class="btn btn-danger" onClick={()=>this.borrar(elemento.codigo)}>Borrar</button>
+              </td>
+            </tr>
+          )
+        })}    
+        </tbody>    
+      </table>
+      </div></center>
+      </div>
+    );
+  }
+  borrar(cod) {
+    var temp = this.state.articulos.filter((el)=>el.codigo !== cod);
+    this.setState({
+      articulos: temp
+    })
+}
+}
+export default App;
+
+
